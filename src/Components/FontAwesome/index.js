9708123bb47e353b996485cedce5ca4d0be6e649
yash140920 <88269072+yash140920@users.noreply.github.com>
2021-12-16 23:11:34 +0530

    Delete src directory

--- src/Components/FontAwesome/index.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {faCrosshairs } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCrosshairs)
--- src/Components/Herosection/Buttons/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../hero.css'
-
-const button = (props)=> {
-    return (
-        <button className={props.classes}>{props.name}</button>
-    )
-}
-export default button
